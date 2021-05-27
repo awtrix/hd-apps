ebf18da923cfc1338fd025f24599636dd8410615
Christopher Mühl <christopher@padarom.xyz>
2021-05-27 16:25:32 +0200

    Move stuff

---
+++ scripts/comment/index.ts
@@ -0,0 +1,9 @@
+// This script is responsible for generating a comment output for any PR.
+//
+// It runs automated tests to make sure that any app changes are according to
+// our contribution guidelines and generates a message text that is sent as a
+// comment to the PR.
+
+let [_bin, _file, app] = process.argv
+
+console.log('Thank you!')
